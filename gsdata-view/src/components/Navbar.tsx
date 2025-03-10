720073ca21ccf55ffd2d6f3338165972d3b7c19a
Suyash Mishra <suyashmishra1008@gmail.com>
2025-03-10 19:14:03 +0530

    logo managed for production

--- gsdata-view/src/components/Navbar.tsx
+++ gsdata-view/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="bg-white shadow-md p-4 flex items-center justify-between fixed top-0 left-0 w-full z-50">
       <div className="flex items-center gap-2">
         <Link to="/">
-          <img src="src/assets/gsynergy-logo.svg" alt="G Synergy Logo" className="h-12 w-auto cursor-pointer" />
+          <img src="/gsynergy-logo.svg" alt="G Synergy Logo" className="h-12 w-auto cursor-pointer" />
         </Link>
         <h1 className="text-3xl font-semibold text-gray-800 absolute left-1/2 transform -translate-x-1/2">
           Data Viewer App
